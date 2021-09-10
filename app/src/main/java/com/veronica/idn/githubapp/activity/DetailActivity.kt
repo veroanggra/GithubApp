4fc2ecfb8922cc0963f6aa24c4d4827f0319bc4d
veroanggra <veronicaputri497@gmail.com>
2021-09-10 14:51:48 +0700

    detail up again

--- app/src/main/java/com/veronica/idn/githubapp/activity/DetailActivity.kt
+++ app/src/main/java/com/veronica/idn/githubapp/activity/DetailActivity.kt
@@ -2,11 +2,38 @@ package com.veronica.idn.githubapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.viewpager2.widget.ViewPager2
 import com.veronica.idn.githubapp.R
+import com.veronica.idn.githubapp.adapter.ViewPagerAdapter
+import com.veronica.idn.githubapp.databinding.ActivityDetailBinding
+import com.veronica.idn.githubapp.viewmodel.DetailViewModel
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var detailBinding: ActivityDetailBinding
+    private lateinit var detailViewModel: DetailViewModel
+    private lateinit var sectionPagerAdapter : ViewPagerAdapter
+    private lateinit var tvFollowers : TextView
+    private lateinit var viewPager : ViewPager2
+    private lateinit var tvFollowing : TextView
+
+    companion object{
+        const val EXTRA_USERNAME = "extra_username"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        detailBinding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(detailBinding.root)
+
+        setViewPager()
+
+    }
+
+    private fun setViewPager() {
+        sectionPagerAdapter = ViewPagerAdapter(this)
+        viewPager = detailBinding.vpDetail
+        viewPager.adapter = sectionPagerAdapter
     }
 }
--- app/src/main/java/com/veronica/idn/githubapp/viewmodel/FollowViewModel.kt
+++ app/src/main/java/com/veronica/idn/githubapp/viewmodel/FollowViewModel.kt
@@ -1,16 +1,22 @@
 package com.veronica.idn.githubapp.viewmodel
 
 import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import com.veronica.idn.githubapp.model.Users
 import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
+import java.lang.Exception
 
 class FollowViewModel : ViewModel() {
     val listFollow = MutableLiveData<ArrayList<Users>>()
+
     fun setListFollow(username: String, page: String, context: Context) {
+        val listUser = ArrayList<Users>()
+
         val client = AsyncHttpClient()
         client.addHeader("Authorization", "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")
         client.addHeader("User-Agent", "request")
@@ -21,22 +27,44 @@ class FollowViewModel : ViewModel() {
             else -> ""
         }
 
-        client.get(url, object : AsyncHttpResponseHandler(){
+        client.get(url, object : AsyncHttpResponseHandler() {
             override fun onSuccess(
                 statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?
+                headers: Array<out Header>,
+                responseBody: ByteArray
             ) {
-                TODO("Not yet implemented")
-            }
+                try {
+                    val result = String(responseBody)
+                    val jsonArray = JSONArray(result)
 
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+
+                        val user = Users()
+                        user.username = jsonObject.getString("login")
+                        user.avatar = jsonObject.getString("avatar_url")
+
+                        listUser.add(user)
+                    }
+                    listFollow.postValue(listUser)
+                } catch (e: Exception) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
             override fun onFailure(
                 statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?,
-                error: Throwable?
+                headers: Array<out Header>,
+                responseBody: ByteArray,
+                error: Throwable
             ) {
-                TODO("Not yet implemented")
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
             }
         })
     }
